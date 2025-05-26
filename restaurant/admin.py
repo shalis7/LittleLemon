a1f80208da2730ac2d3ee3c2518c47dccc1f8f44
shalis <natashaalis089@gmail.com>
2025-05-26 19:05:03 +0800

    Add models and admin configurations for Menu and Booking

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from restaurant.models import Menu, Booking
 
 # Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'inventory')
+    search_fields = ('title',)
+    list_filter = ('price',)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'no_of_guests', 'booking_date')
+    search_fields = ('name',)
+    list_filter = ('booking_date',)
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Booking, BookingAdmin)
