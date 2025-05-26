ae7bf9f2b557bb276b9a372f5007d08c8c844a46
shalis <natashaalis089@gmail.com>
2025-05-26 19:46:17 +0800

    Add REST framework integration with Menu and User serializers, and update URL routing

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, 
+                       generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
