87e3458e66a774f3d019069f0cf40683e926e8d3
shalis <natashaalis089@gmail.com>
2025-05-27 18:03:28 +0800

    Add Booking API integration with serializers and views, and update URL routing

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Booking
 from django.contrib.auth.models import User
 
 class MenuSerializer(serializers.ModelSerializer):
@@ -12,3 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking  # Assuming Booking is also a model in your app
+        fields = '__all__'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Menu
-from .serializers import MenuSerializer
+from .serializers import MenuSerializer, BookingSerializer
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .models import Booking
 
 # Create your views here.
 def index(request):
@@ -17,3 +19,8 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView,
                        generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    # permission_classes = [permissions.IsAuthenticated]
